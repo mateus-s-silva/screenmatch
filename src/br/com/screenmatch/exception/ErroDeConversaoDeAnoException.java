a257f82a30722959ce852507a6045fc88b9fb7ee
mateus-s-silva <mattahyah@gmail.com>
2023-10-03 01:28:25 -0300

    Consumindo API e escrevendo arquivos

---
+++ src/br/com/screenmatch/exception/ErroDeConversaoDeAnoException.java
@@ -0,0 +1,13 @@
+package br.com.screenmatch.exception;
+
+public class ErroDeConversaoDeAnoException extends RuntimeException {
+    private String mensagem;
+    public ErroDeConversaoDeAnoException(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    @Override
+    public String getMessage() {
+        return this.mensagem;
+    }
+}
